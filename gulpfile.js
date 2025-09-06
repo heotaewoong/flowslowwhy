66c8ace64b2fdae70c0920901e7d20e0db6221b1
ihtwandy <ihtwandy@naver.com>
2025-09-06 22:42:20 +0900

    Fix typo: equire -> require in gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 // node.js Packages / Dependencies
-const gulp          = equire('gulp');
-const sass          = equire('gulp-sass')(require('sass'));
+const gulp          = require('gulp');
+const sass          = require('gulp-sass')(require('sass'));
 const uglify        = require('gulp-uglify');
 const rename        = require('gulp-rename');
 const concat        = require('gulp-concat');
